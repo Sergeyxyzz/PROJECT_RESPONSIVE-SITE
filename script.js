9ec2b750eae629608d472ff986b2472cc6656d27
Sergey <sergeyxyzz@yandex.ru>
2023-06-13 19:55:30 +0300

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggler = document.querySelectorAll('#menu_toggle')
+const header = document.querySelector('header')
+
+menuToggler.forEach(toggler => {
+    toggler.addEventListener('click', () => header.classList.toggle('showMenu'))
+})
